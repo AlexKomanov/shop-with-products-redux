3bda88d17e0584e2b02431278118d7b65f178291
Alex <akomanov@seatgeek.com>
2021-08-16 23:40:49 +0300

    Adding basic routing

--- src/components/app-component/App.jsx
+++ src/components/app-component/App.jsx
@@ -1,10 +1,20 @@
-import Header from '../header-component/Header';
-import './App.css';
+import Header from "../header-component/Header";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Products from "./../products-component/Products";
+import ProductDetail from "../product-detail-component/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Products} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
